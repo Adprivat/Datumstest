e00ac36b533cd8b2ead32ad79b980dda398b6f7b
ASchultz <131669459+Adprivat@users.noreply.github.com>
2025-02-12 08:22:12 +0100

    Robustheit erhöht

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from datetime import datetime
 
-name =["andi"]
-
 def get_date_from_user():
-    date_str = input("Bitte geben Sie ein Datum im Format TT.MM.JJJJ ein: ")
+    date_str = input("Bitte geben Sie ein Datum im Format TT.MM.JJJJ ein: ").strip()
     try:
-        date_obj = datetime.strptime(date_str, "%d.%m.%Y") # Versucht, die Eingabe in ein datetime-Objekt zu konvertieren
-        print(f"Das eingegebene Datum ist: {date_obj.strftime('%d.%m.%Y')}") # Ausgabe
+        date_obj = datetime.strptime(date_str, "%d.%m.%Y")  # Konvertiert die bereinigte Eingabe in ein datetime-Objekt
+        print(f"Das eingegebene Datum ist: {date_obj.strftime('%d.%m.%Y')}")
     except ValueError:
-        print("Fehlerhafte Eingabe. Bitte geben Sie ein gültiges Datum im Format TT.MM.JJJJ ein.")   # Gibt eine Fehlermeldung aus, wenn die Eingabe nicht konvertiert werden kann
+        print("Fehlerhafte Eingabe. Bitte geben Sie ein gültiges Datum im Format TT.MM.JJJJ ein.")
 
 if __name__ == "__main__":
     get_date_from_user()
